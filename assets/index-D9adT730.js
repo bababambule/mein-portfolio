2d31bb735a6459e74a147ff409498b5d6240d804
Baba Bambule <bababambule@proton.me>
2025-11-16 01:03:11 +0100

    Updates

---
+++ assets/index-D9adT730.js
@@ -0,0 +1,1 @@
+import"./mobileMenu-Dnc_SqrW.js";import"./main-DA1WSotE.js";import"./main-BqmmClzg.js";import"./Hero-Ddj9l2g4.js";import"./fa7-brands_dribbble-square-C3JX79Ao.js";import"./Footer-DhjWGYHu.js";
--- assets/main-DA1WSotE.js
+++ assets/main-DA1WSotE.js
@@ -1,4 +1,4 @@
-import{c as p,i as r}from"./mobileMenu-Dnc_SqrW.js";import{c,a as g}from"./Hero-Ddj9l2g4.js";import{c as n,b as v,f as u}from"./Footer-BbDVZsfo.js";const l="/assets/card-digital-prospekt-GvpxFVwl.webp";function m(){const e=document.createElement("section");e.setAttribute("id","projects"),e.className="bg-amber-600 py-12 flex flex-col gap-24 text-amber-50";const a=document.createElement("div");a.className="flex flex-col gap-0 px-6",a.innerHTML=`
+import{c as p,i as r}from"./mobileMenu-Dnc_SqrW.js";import{c,a as g}from"./Hero-Ddj9l2g4.js";import{c as n,b as v,f as u}from"./Footer-DhjWGYHu.js";const l="/assets/card-digital-prospekt-GvpxFVwl.webp";function m(){const e=document.createElement("section");e.setAttribute("id","projects"),e.className="bg-amber-600 py-12 flex flex-col gap-24 text-amber-50";const a=document.createElement("div");a.className="flex flex-col gap-0 px-6",a.innerHTML=`
     <div class="flex gap-4  justify-between -mb-8 z-10 relative">
         <div>
             <p class="text-6xl font-black uppercase text-right drop-shadow-lg">Digi<br>tal</p>
